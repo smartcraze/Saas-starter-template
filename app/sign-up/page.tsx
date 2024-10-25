87d908edbd237e05e430701da38cdff2af90b2de
smartcraze <smartcraze354@gmail.com>
2024-10-26 01:03:19 +0530

    Added shadcn

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useSignUp } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
